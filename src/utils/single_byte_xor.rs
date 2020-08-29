5c898778c0be19428004ee495f86e8675019d9cf
proby <proby@users.noreply.github.com>
2020-08-29 21:00:36 +1200

    change singe_byte_xor impl for 10x speedup

--- src/utils/single_byte_xor.rs
+++ src/utils/single_byte_xor.rs
@@ -1,36 +1,23 @@
 use super::{hex, scorer, util, xor_score};
 
-fn xor_decrypt_and_score(hex_str_as_bytes: Vec<u8>, byte_to_test: u8) -> (f32, Vec<u8>) {
-    let other_vec: Vec<u8> = vec![byte_to_test; hex_str_as_bytes.len()];
-    let xored_bytes = util::xor_byte_vecs(hex_str_as_bytes, other_vec);
-
-    let score = scorer::score_for(xored_bytes.clone());
-
-    (score, xored_bytes)
-}
-
 pub fn decrypt(hex_str: &str) -> xor_score::XorScore {
     let hex_str_as_bytes: Vec<u8> = hex::decode(hex_str);
 
     let mut best = xor_score::XorScore::default();
     for byte_to_test in 0..255 {
-        let (score, xored_bytes) = xor_decrypt_and_score(hex_str_as_bytes.clone(), byte_to_test);
+        let other_vec: Vec<u8> = vec![byte_to_test; hex_str_as_bytes.len()];
+        let xored_bytes = util::xor_byte_vecs(hex_str_as_bytes.clone(), other_vec);
 
-        if score > best.score {
-            best.decoding_byte = byte_to_test;
-            best.score = score;
-            best.xored_bytes = xored_bytes;
+        if let Ok(utf_str) = String::from_utf8(xored_bytes.clone()) {
+            let score = scorer::score_for(xored_bytes.clone());
+            if score > best.score {
+                best.decoding_byte = byte_to_test;
+                best.score = score;
+                best.xored_bytes = xored_bytes;
+                best.decoded_string = utf_str.trim().to_string();
+            }
         }
     }
-
-    let utf8_str = String::from_utf8(best.xored_bytes.clone());
-    if utf8_str.is_err() {
-        best.score = 0.0;
-        best.decoded_string = String::from("NOT UTF_8");
-        return best;
-    }
-
-    best.decoded_string = utf8_str.unwrap().trim().to_string();
 
     best
 }
