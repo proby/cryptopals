dd0991152b7c9a0dbff335f82b5767179dcf184b
proby <proby@users.noreply.github.com>
2020-09-12 17:50:05 +1200

    add pkcs7 tests

--- src/utils/pkcs_padding.rs
+++ src/utils/pkcs_padding.rs
@@ -17,6 +17,13 @@ mod tests {
             b"YELLOW SUBMARINE\x04\x04\x04\x04"
         );
         assert_eq!(pad(b"YELLOW SUBMARINE!!!", 20), b"YELLOW SUBMARINE!!!\x01");
+        assert_eq!(
+            pad(
+                b"blueBOATSandGreen submarine&YELLOW SUBMARINESANDBLACK666",
+                20
+            ),
+            "blueBOATSandGreen submarine&YELLOW SUBMARINESANDBLACK666\x04\x04\x04\x04".as_bytes()
+        );
     }
 
     #[test]
@@ -24,6 +31,18 @@ mod tests {
         assert_eq!(
             pad(b"YELLOW SUBMARINE", 8),
             b"YELLOW SUBMARINE\x08\x08\x08\x08\x08\x08\x08\x08"
-        )
+        );
+        assert_eq!(
+            pad(b"YELLOW SUBMARINE", 16),
+            b"YELLOW SUBMARINE\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10"
+        );
+        assert_eq!(
+            pad(b"Green submarine&YELLOW SUBMARINE", 8),
+            "Green submarine&YELLOW SUBMARINE\x08\x08\x08\x08\x08\x08\x08\x08".as_bytes()
+        );
+        assert_eq!(
+            pad(b"Green submarine&YELLOW SUBMARINE", 16),
+            "Green submarine&YELLOW SUBMARINE\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10".as_bytes()
+        );
     }
 }
